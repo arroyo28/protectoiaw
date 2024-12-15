a07452aa7c9e53c49dd865cf6a4dfa25aae6962e
arroyo28 <aarrfer2asir@gmail.com>
2024-12-15 09:37:21 +0100

    insertar2.php

--- insertar2.php
+++ insertar2.php
@@ -1,4 +1,9 @@
 <?php
+session_start();
+if (!isset($_SESSION['user_logged_in']) || $_SESSION['user_logged_in'] !== true) {
+    header('Location: login-1.php'); // Redirige si no está autenticado
+    exit;
+}
 $servername = "192.168.6.12";
 $username = "aaf";
 $password = "12345";
